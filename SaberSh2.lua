7140474fd29170e23527711b1198784ac14f7f61
Denmm-9 <raikonen20039@gmail.com>
2025-10-01 20:38:18 -0300

    SaberSh2.lua

--- SaberSh2.lua
+++ SaberSh2.lua
@@ -70,7 +70,7 @@ local function expandAllPlayerHitboxes()
                             Color = hrp.Color
                         }
                     end
-                    hrp.Size = Vector3.new(5,5,5)
+                    hrp.Size = Vector3.new(4,4,4)
                     hrp.CanCollide = false
                     hrp.CanTouch = false
                     hrp.Transparency = 0.9
