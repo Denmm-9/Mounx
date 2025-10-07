2f89741181f0f967666b12e814f74aa2d60fb44e
Denmm-9 <raikonen20039@gmail.com>
2025-10-07 00:47:55 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,13 +3,25 @@ if not game:IsLoaded() then
     task.wait(1)
 end
 
-local ListURl = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/Game_list.lua"
+local ListURl = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/Game_list.lua"
 local games = loadstring(game:HttpGet(ListURl))()
 
+local UserInputService = game:GetService("UserInputService")
+local isMobile = UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled
+
+local deviceType = isMobile and "mobile" or "pc"
+print("Device detected:", deviceType)
+
 local success, errorMsg = pcall(function()
-    for placeId, scriptUrl in pairs(games) do
+    for placeId, data in pairs(games) do
         if game.PlaceId == placeId then
-            loadstring(game:HttpGet(scriptUrl))()
+            local scriptUrl = data[deviceType]
+            if scriptUrl then
+                print("Loading script:", scriptUrl)
+                loadstring(game:HttpGet(scriptUrl))()
+            else
+                warn("No script found for this device type.")
+            end
         end
     end
 end)
