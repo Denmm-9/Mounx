4eab378107dea660e9186747a28964888e2b9aeb
Denmm-9 <raikonen20039@gmail.com>
2025-10-07 00:23:09 -0300

    loader.lua

---
+++ loade.lua
@@ -0,0 +1,19 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+    task.wait(1)
+end
+
+local ListURl = "https://github.com/Denmm-9/Mounx/blob/main/Game_list.lua"
+local games = loadstring(game:HttpGet(ListURl))()
+
+local success, errorMsg = pcall(function()
+    for placeId, scriptUrl in pairs(games) do
+        if game.PlaceId == placeId then
+            loadstring(game:HttpGet(scriptUrl))()
+        end
+    end
+end)
+
+if not success then
+    warn(errorMsg)
+end
