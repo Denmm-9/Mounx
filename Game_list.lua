c129493c1adb7c3c3ef8cc87f66f52e5ecd0d85a
Denmm-9 <raikonen20039@gmail.com>
2025-10-07 00:43:29 -0300

    Update Game_list.lua

--- Game_list.lua
+++ Game_list.lua
@@ -1,5 +1,11 @@
 local games = {
     [11157166793] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/AntWar.lua",
+    [117701570624742] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/BladesBuffonery.lua",
+    [15118223068] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/ChooseAnime.lua",
+    [81734311524009] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/DungeonHeroes.lua",
+    [10219766033] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/Panik.lua",
+    [6607797163] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/RagdollB.lua",
+
     
 }
 
