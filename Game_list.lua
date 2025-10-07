6d547ac6987b98db852e242593c3bf9e5fa01be0
Denmm-9 <raikonen20039@gmail.com>
2025-10-07 00:52:45 -0300

    Update Game_list.lua

--- Game_list.lua
+++ Game_list.lua
@@ -1,8 +1,8 @@
 local games = {
     [12625784503] = {
         pc = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/SaberSh2.lua",
-        mobile = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/SaberSh.lua",
-    }
+        mobile = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/SaberSh.lua"
+    },
     [11157166793] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/AntWar.lua",
     [117701570624742] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/BladesBuffonery.lua",
     [15118223068] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/ChooseAnime.lua",
