e7aa40fb9fa161788d905dd3fe79a0bb20df9407
Denmm-9 <raikonen20039@gmail.com>
2025-10-07 00:53:47 -0300

    Update Game_list.lua

--- Game_list.lua
+++ Game_list.lua
@@ -1,16 +1,15 @@
 local games = {
     [12625784503] = {
-        pc = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/SaberSh2.lua"
-        mobile = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/SaberSh.lua"
+        pc = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/SaberSh2.lua",
+        mobile = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/SaberSh.lua"
     },
-    [11157166793] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/AntWar.lua",
-    [117701570624742] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/BladesBuffonery.lua",
-    [15118223068] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/ChooseAnime.lua",
-    [81734311524009] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/DungeonHeroes.lua",
-    [10219766033] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/Panik.lua",
-    [6607797163] = "https://raw.githubusercontent.com/Denmm-9/Mounx/refs/heads/main/RagdollB.lua",
-
     
+    [11157166793] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/AntWar.lua",
+    [117701570624742] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/BladesBuffonery.lua",
+    [15118223068] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/ChooseAnime.lua",
+    [81734311524009] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/DungeonHeroes.lua",
+    [10219766033] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/Panik.lua",
+    [6607797163] = "https://raw.githubusercontent.com/Denmm-9/Mounx/main/RagdollB.lua"
 }
 
 return games
