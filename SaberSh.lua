b1da79387bfc5df08439211b73fc35005a615633
Denmm-9 <raikonen20039@gmail.com>
2025-12-29 02:43:42 -0300

    Update SaberSh.lua

--- SaberSh.lua
+++ SaberSh.lua
@@ -68,7 +68,7 @@ local HitboxCaster = require(ReplicatedStorage.LightsaberModules.RaycastHitbox.H
 ----------------------------------------------------------------
 -- Config
 ----------------------------------------------------------------
-local MAX_DISTANCE = 6
+local MAX_DISTANCE = 7
 local SCAN_INTERVAL = 0.01
 local HIT_COOLDOWN = 0 
 local lastHit = 0
